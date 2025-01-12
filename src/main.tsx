69bb8b5ed7da282a6421ba8520511684f591c64a
Damian Tarnawski <gthetarnav@gmail.com>
2025-01-12 22:23:29 +0100

    Add tldraw

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import * as App from './app'
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+import 'tldraw/tldraw.css'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <App.App />
+    </React.StrictMode>,
 )
