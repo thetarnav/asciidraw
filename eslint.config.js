e76f742e83027ed33f2b608e06374bcab106efbb
Damian Tarnawski <gthetarnav@gmail.com>
2025-01-13 08:54:06 +0100

    Remove eslint-plugin-react-refresh

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from '@eslint/js'
 import globals from 'globals'
 // @ts-expect-error
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -16,7 +15,6 @@ export default tseslint.config(
         },
         plugins: {
             'react-hooks': reactHooks,
-            'react-refresh': reactRefresh,
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
